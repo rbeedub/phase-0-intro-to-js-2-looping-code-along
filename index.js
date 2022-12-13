0ebdb369e9eb6537460c578faac23200b0627412
Rebecca Wollard <rbwollard@gmail.com>
2022-12-13 16:54:01 -0700

    commit tree 545f9b01fd2c68bcc83407e83f497bd8310fc0aa

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+const friends = [];
+const holiday = [];
+
+
+function writeCards(friends,holiday){
+  const messages = []
+  
+  for (let i=0; i < friends.length; i++) {
+    const message= (`Thank you, ${friends[i]}, for the wonderful ${holiday} gift!`);
+    messages.push(message);
+  }
+  return messages;
+}
+  console.log (writeCards(["Guadalupe", "Ollie","Aki"], "birthday"));
+
+
+let i = [];
+function countDown(i) {
+  while (i >= 0) {
+  console.log(i--);
+} 
+} 
+countDown(10);
